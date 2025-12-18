e929468d747abc4af80a4c6da4eacf750f450878
Allieniaszxy <alakaalamin40@gmail.com>
2025-12-18 05:56:42 -1100

    done with the resumecard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,38 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constant/index.";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "AI-Resume-Analyzer" },
+    {
+      name: "description",
+      content: "Smart feedback for getting your dream job",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <p>
+            Review your applications and get personalized feedback to improve
+            your resume and increase your chances of landing your dream job.
+          </p>
+        </div>
+        {resumes.length > 0 && (
+          <div className="resume-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
